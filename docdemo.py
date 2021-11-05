1cdc800dced66cee2bac7a74ae47a54a4e6cc5d2
chidanandpujar <46497833+chidanandpujar@users.noreply.github.com>
2021-11-05 15:18:59 +0530

    Create docdemo.py
    Inside docstrdemo
    Following is docstring
    
            Docstring demo doc string

---
+++ docdemo.py
@@ -0,0 +1,10 @@
+def docstrdemo():
+    """
+        Docstring demo doc string
+    """
+    print("Inside docstrdemo")
+    print("Following is docstring")
+    print(docstrdemo.__doc__)
+
+
+docstrdemo()
