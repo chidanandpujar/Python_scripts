cf01955546dd447d1594bea7553d8ce04733274b
chidanandpujar <46497833+chidanandpujar@users.noreply.github.com>
2021-11-13 17:35:03 +0530

    Create 5inh.py
    True
    True
    True
    False
    True
    False
    True

---
+++ 5inh.py
@@ -0,0 +1,15 @@
+class A:
+    pass
+class B(A):
+    pass
+
+b = B()
+print(isinstance(b,B))
+print(isinstance(b,A))
+a=5
+print(isinstance(a,int))
+print(isinstance(a,float))
+
+print(issubclass(B,A))
+print(issubclass(A,B))
+print(issubclass(bool,int))
