1d121caa3c7b4a9859d84165b48bc3fd221dd666
chidanandpujar <46497833+chidanandpujar@users.noreply.github.com>
2021-11-14 16:38:38 +0530

    Create multiexcepts.py
    Enter number:0
    Device by Zero attempted

---
+++ multiexcepts.py
@@ -0,0 +1,9 @@
+try:
+    n = int(input("Enter number:"))
+    m = 100/n
+except ValueError:
+    print("Value is not integer type")
+except ZeroDivisionError:
+    print("Device by Zero attempted")
+else:
+    print(n)
