0ea260ab1026913eb0a9b8bac67dc9803cfb74f1
chidanandpujar <46497833+chidanandpujar@users.noreply.github.com>
2021-11-20 19:29:33 +0530

    Update argthread.py
    Finished simple thread!!
     2

--- argthread.py
+++ argthread.py
@@ -2,6 +2,6 @@ import threading
 def mythread(arg):
     print("Finished simple thread!!\n",arg)
 
-t = threading.Thread(target=mythread(2))
+t = threading.Thread(target=mythread,args=(2,))
 t.start()
 t.join()
