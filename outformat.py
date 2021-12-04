f1e3720f0729a5dc20577cbe2f439414dd5b2b83
chidanandpujar <46497833+chidanandpujar@users.noreply.github.com>
2021-12-04 17:39:37 +0530

    Create outformat.py
    India scored 10 goals
    Seenu has 10 mangoes
    Prabhu's full name is Prabhu Gudi
    Lastname Gudi Firstname Prabhu
    Firstname Prabhu Lastname Gudi
    float for 18 is 18.000000
    2.69

---
+++ outformat.py
@@ -0,0 +1,9 @@
+print("India scored {} goals".format(10))
+mg=10
+print("Seenu has {} mangoes".format(mg))
+name = "Prabhu's full name is {} {}"
+print(name.format("Prabhu", "Gudi"))
+print("Lastname {1} Firstname {0}".format("Prabhu","Gudi"))
+print("Firstname {0} Lastname {1}".format("Prabhu","Gudi"))
+print("float for 18 is {0:f}".format(18))
+print("{0:.2f}".format(2.689))
