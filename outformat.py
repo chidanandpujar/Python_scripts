855efd3eaf6cc8523154585af3b609cd4a40de41
chidanandpujar <46497833+chidanandpujar@users.noreply.github.com>
2021-12-04 17:49:43 +0530

    Update outformat.py
    India scored 10 goals
    Seenu has 10 mangoes
    Prabhu's full name is Prabhu Gudi
    Lastname Gudi Firstname Prabhu
    Firstname Prabhu Lastname Gudi
    float for 18 is 18.000000
    2.69
    1
    Number    10 String Hi                  !!
    Number 10    String Hi                  !!
    Number    10 String                   Hi!!
    Number  10   String          Hi         !!

--- outformat.py
+++ outformat.py
@@ -7,3 +7,8 @@ print("Lastname {1} Firstname {0}".format("Prabhu","Gudi"))
 print("Firstname {0} Lastname {1}".format("Prabhu","Gudi"))
 print("float for 18 is {0:f}".format(18))
 print("{0:.2f}".format(2.689))
+print("{0:.0f}".format(1.234))
+print("Number {0:5} String {1:20}!!".format(10,"Hi"))
+print("Number {0:<5} String {1:<20}!!".format(10,"Hi")) #left justified
+print("Number {0:>5} String {1:>20}!!".format(10,"Hi")) #Right justified
+print("Number {0:^5} String {1:^20}!!".format(10,"Hi")) #Cetre justified
